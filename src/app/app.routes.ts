c1c6d237bead4001d4f590c6669cb3d35de47e8c
Cartago <cartago.relaxingcup@gmail.com>
2023-12-09 00:35:31 +0100

    Comenzamos a crear un tetris

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'game',
+    async loadComponent() {
+      return (await import('./pages/game/game.component')).GameComponent;
+    },
+  },
+  { path: '**', redirectTo: 'game' },
+];
