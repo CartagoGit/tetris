decfa6a9631c835bac469da127f9ec1e130123f9
Cartago <cartago.relaxingcup@gmail.com>
2023-12-09 04:28:29 +0100

    Comenzamos la implementacion de la creacion de piezas

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule, JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Piece } from '../../shared/models/piece.model';
+import { StateService } from '../../shared/services/state.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-table',
@@ -13,4 +16,30 @@ export class TableComponent {
   public rows = 20;
   public columns = 10;
   public table = new Array(this.rows).fill(new Array(this.columns).fill(0));
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(public stateSvc: StateService) {
+    this._createSubscriptions();
+    const piece = new Piece();
+    console.log(piece);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => !sub.closed && sub.unsubscribe());
+  }
+
+  // ANCHOR  : Methods
+
+  private _createSubscriptions(): void {
+    const subGameStart = this.stateSvc.gameStart$.subscribe((gameStart) => {
+      if (!gameStart) return;
+      this._createTable();
+    });
+    this.subscriptions.push(subGameStart);
+  }
+
+  private _createTable(): void {
+    this.table = new Array(this.rows).fill(new Array(this.columns).fill(0));
+  }
 }
