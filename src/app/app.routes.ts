f118896bd0583b8efa74fe5b8e5a20f730743365
Cartago <cartago.relaxingcup@gmail.com>
2023-12-10 17:14:48 +0100

    Parcheamos para netlify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // {
+  //   path: 'game',
+  //   async loadComponent() {
+  //     return (await import('./pages/game/game.component')).GameComponent;
+  //   },
+  // },
+  // { path: '**', redirectTo: 'game' },
   {
-    path: 'game',
+    path: '',
+    pathMatch: 'full',
     async loadComponent() {
       return (await import('./pages/game/game.component')).GameComponent;
     },
   },
-  { path: '**', redirectTo: 'game' },
+  { path: '**', redirectTo: '' },
 ];
